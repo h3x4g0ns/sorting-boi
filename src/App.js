fe2ef0e218c4a9b65647e1c7d7d5b1a7b6b50505
Ekansh Agrawal <agrawalekansh@gmail.com>
2022-06-14 16:01:09 -0700

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-gray-800 h-screen">
+      <header className="font-mono text-4xl text-slate-50 pt-10 pl-10">
+        <h1>Sorting Boi</h1>
       </header>
+      <div className="flex pt-10 px-10">
+        <div className="bg-red-800 px-1 w-3/12">
+          <h1>test1</h1>
+        </div>
+        <div className="bg-purple-800 pr-10 w-9/12">
+          <h1>test2</h1>
+        </div>
+      </div>
     </div>
   );
 }
