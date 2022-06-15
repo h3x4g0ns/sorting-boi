b6da91342ac095834869d3cc4983fecf40ee496c
Ekansh Agrawal <agrawalekansh@gmail.com>
2022-06-14 18:58:40 -0700

    temp state variables and infastructure

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -4,12 +4,43 @@ import Component from 'react';
 export default class Visualizer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      array: [],
+      algo: "bubble",
+      size: 100,
+      max: 100
+    };
+
+    this.generateArray();
   }
+
+  generateArray = () => {
+    console.log("generating");
+    for(let i = 0; i < this.state.size; i++) {
+      this.state.array[i] = Math.random() * this.state.max;
+    }
+  }
+
+  /*sortArray = () => {
+    switch(this.state.algo) {
+      case "bubble":
+        bubbleSort();
+        break;
+      case "quick":
+        quicksort();
+        break;
+      default:
+        bubbleSort();
+        break;
+    }
+  }*/
 
   render() {
     return(
-      <div></div>
+      <div className="flex">
+        {this.state.array.map((bar) => 
+          <div className="bg-green-30 h-1/4"></div>)}
+      </div>
     )
   }
 }
