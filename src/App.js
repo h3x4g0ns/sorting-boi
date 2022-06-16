23d01b357b25fba56b4beba9dfeb9fbf9aad27f6
Ekansh Agrawal <agrawalekansh@gmail.com>
2022-06-16 13:28:23 -0700

    added input buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import { Component } from 'react';
 import Visualizer from "./Visualizer";
+import Select from "react-select";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      algo: "bubble",
+      startSorting: false,
+    }
+    this.options = [
+      { value: 'bubble', label: 'Bubble Sort' },
+      { value: 'quick', label: 'Quick Sort' },
+      { value: 'merge', label: 'Merge Sort' },
+      { value: 'heap', label: 'Heap Sort' },
+      { value: 'insertion', label: 'Insertion Sort' },
+      { value: 'selection', label: 'Selection Sort' }
+    ];
+  }
+
+  triggerSorting = () => {
+    console.log(this.state.algo);
+    this.setState({startSorting: true});
+  }
+  
   render() {
     return (
       <div className="bg-gray-800 h-screen">
@@ -9,11 +31,18 @@ export default class App extends Component {
           <h1>Sorting Boi</h1>
         </header>
         <div className="flex pt-10 px-10">
-          <div className="bg-red-800 px-1 w-3/12">
-            <h1>test1</h1>
+          <div className="px-1 py-5 w-1/6">
+            <Select options={this.options} 
+                    value={this.state.algo}
+                    onChange={(value) => this.setState({algo: value})}/>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white mt-5 font-bold py-2 px-4 rounded-full" 
+                    onClick={this.triggerSorting}>
+              Sort
+            </button>
           </div>
-          <div className="bg-purple-800 pr-10 w-9/12 mr-2">
-            <Visualizer/ >
+          <div className="pr-10 w-5/6 mr-2">
+            <Visualizer algo={this.state.algo}
+                        startSorting={this.state.startSorting} / >
           </div>
         </div>
       </div>
